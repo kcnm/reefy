5bfa7289a9b97f8d4b49f043e2407570122be963
kcnm <devil.alex.dante@gmail.com>
2016-09-21 23:55:05 -0700

    Fix travis script

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,12 +6,6 @@ import Reefy from './components/Reefy';
 // Import CSS module.
 import './stylesheet.css';
 
-// Default input.
-let config = {
-  fontSize: 12,
-  fontFamily: 'monospace',
-  lineHeight: 14
-};
 
 // Renders reefy editor.
 ReactDOM.render(
